1a004ab27bbccaea3980763d591b3559af98c71f
InfinityITDept <juan@usg-group.net>
2020-10-20 09:10:12 -0400

    Cards update

--- src/layout/components/InfoCards.js
+++ src/layout/components/InfoCards.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import "./InfoCards.scss";
+import Placeholder from "./images/placeholder.jpg";
 
 export default class InfoCards extends Component {
     render() {
         return (
-            <div>
-                These would be the info cards
+        <div className="Cards">
+            <div className="Cards-container">
+                <div className="Cards-img-col">
+                    <img className="Cards-img" src={Placeholder} alt="Placeholder" />
+                </div>
+                <div className="Cards-text-col">
+                    <h1 className="Cards-title">Title</h1>
+                    <p className="Cards-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, dui vel sollicitudin faucibus, ipsum magna vehicula augue, quis pulvinar urna justo nec mauris. Duis eu libero sed libero porta pharetra.</p>
+                </div>
             </div>
+
+            <div className="Cards-container">
+                <div className="Cards-img-col">
+                    <img className="Cards-img" src={Placeholder} alt="Placeholder" />
+                </div>
+                <div className="Cards-text-col">
+                    <h1 className="Cards-title">Title</h1>
+                    <p className="Cards-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, dui vel sollicitudin faucibus, ipsum magna vehicula augue, quis pulvinar urna justo nec mauris. Duis eu libero sed libero porta pharetra.</p>
+                </div>
+            </div>
+
+            <div className="Cards-container">
+                <div className="Cards-img-col">
+                    <img className="Cards-img" src={Placeholder} alt="Placeholder" />
+                </div>
+                <div className="Cards-text-col">
+                    <h1 className="Cards-title">Title</h1>
+                    <p className="Cards-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, dui vel sollicitudin faucibus, ipsum magna vehicula augue, quis pulvinar urna justo nec mauris. Duis eu libero sed libero porta pharetra.</p>
+                </div>
+            </div>
+        </div>
         )
     }
 }
