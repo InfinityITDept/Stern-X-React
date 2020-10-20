bce28f9aae7523a8df0773de2ab4687fdc5bf9a9
adela-usg <adela@usg-group.net>
2020-10-20 09:05:41 -0400

    html navbar

--- src/layout/components/Nav.js
+++ src/layout/components/Nav.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import Navbar from "react-bootstrap/Navbar";
 import "./Nav.scss";
 
 
 export default class Nav extends Component {
     render() {
         return (
-            <div>
-                Navbar
-            </div>
+            <Navbar collapseOnSelect className="pl-3 pr-3" expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="www.google.com">React-Bootstrap</Navbar.Brand>
+
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link" href="www.google.com">PRODUCTS</a>
+                        <a className="nav-link" href="www.google.com">CONTACT US</a>
+                    </div>
+                </div>
+            </Navbar>
         )
     }
 }
