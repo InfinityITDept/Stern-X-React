6659ec7929170e99bdf9caf0a085714d4697af68
InfinityITDept <juan@usg-group.net>
2020-10-23 08:31:33 -0400

    Banner

--- src/layout/components/Banner.js
+++ src/layout/components/Banner.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import "./Banner.scss";
+import Carousel from 'react-bootstrap/Carousel';
+import Last from "./images/Resize-01.jpg";
 
 export default class Banner extends Component {
     render() {
         return (
             <div id="banner">
-                This is where Banner will go
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                        className="img-fluid d-block w-100"
+                        src={Last}
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1000}>
+                        <img
+                        className="img-fluid d-block w-100"
+                        src={Last}
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
             </div>
         )
     }
