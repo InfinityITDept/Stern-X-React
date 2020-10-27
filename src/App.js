ba06fbcdf670c9c38e8e265d2cd4eff2309540af
Juan Pablo <jpluncumilla@gmail.com>
2020-10-26 22:23:00 -0400

    switch routes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import Products from "./layout/Products"
 import Nav from "./layout/components/Nav";
 import Footer from "./layout/components/Footer";
 import SingleProduct from './layout/SingleProduct';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Landing />
+      <Router>
+        <Nav />
+      <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/single-product" component={SingleProduct} />
+      </Switch>
       <Footer />
+      </Router>
     </div>
   );
 }
