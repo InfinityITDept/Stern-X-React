d82b1da82b763370eabe4a15cb68b61920c97f97
InfinityITDept <juan@usg-group.net>
2020-10-27 10:04:32 -0400

    10.27 at 10AM update

--- src/layout/components/Reviews.js
+++ src/layout/components/Reviews.js
@@ -8,6 +8,8 @@ import Headshot3 from "./images/Headshot3.jpg";
 export default class Reviews extends Component {
     render() {
         return (
+            <div>
+                <h1 className="Reviews-title">Reviews</h1>
             <div className="Reviews">
                 <div className="Reviews-container">
                     <div className="Reviews-col">
@@ -15,8 +17,7 @@ export default class Reviews extends Component {
                             <img src={Headshot2} alt="alt" className="Reviews-img"/>
                         </div>
                         <div className="Reviews-text-col">
-                            <h1 className="Reviews-text-title">Reviews</h1>
-                            <hr className="Reviews-hr" />
+
                             <p className="Reviews-text-paragraph">“Easy to use and gentle on my hands! I keep an individual bottle with me whenever I am on the go wherever I go.”</p>
                         </div>
                     </div>
@@ -28,8 +29,7 @@ export default class Reviews extends Component {
                             <img src={Headshot3} alt="alt" className="Reviews-img"/>
                         </div>
                         <div className="Reviews-text-col">
-                            <h1 className="Reviews-text-title">Reviews</h1>
-                            <hr className="Reviews-hr" />
+
                             <p className="Reviews-text-paragraph">“I originally tried this product at home and was well satisfied with it. I now keep a bottle in my desk at the office for my colleagues to use. I would definitely recommend it.”</p>
                         </div>
                     </div>
@@ -41,13 +41,13 @@ export default class Reviews extends Component {
                             <img src={Headshot1} alt="alt" className="Reviews-img"/>
                         </div>
                         <div className="Reviews-text-col">
-                            <h1 className="Reviews-text-title">Reviews</h1>
-                            <hr className="Reviews-hr" />
+
                             <p className="Reviews-text-paragraph">“I switched over to this product recently while looking for a strong solution to kill bacteria at home. Doing research on the ingredients I found this bleach to be one of the more effective ones to disinfect.”</p>
                         </div>
                     </div>
                 </div>
             </div> 
+            </div>
         )
     }
 }
