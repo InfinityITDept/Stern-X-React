29c55c7a60dfa869c5ea52084a3fd37c9820c50b
InfinityITDept <juan@usg-group.net>
2020-11-03 11:20:01 -0500

    removed server file

--- src/layout/Contact.js
+++ src/layout/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import "./Contact.scss";
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
@@ -6,46 +6,101 @@ import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { ThemeProvider } from 'react-bootstrap';
 
-export default function Contact() {
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-  
-    const submitRequest = async (e) => {
-      e.preventDefault();
-      console.log({ email, message });
-      const response = await fetch("/access", { 
-        method: 'POST', 
-        headers: { 
-            'Content-type': 'application/json'
-        }, 
-        body: JSON.stringify({email, message}) 
-    }); 
-      const resData = await response.json(); 
-      if (resData.status === 'success'){
-        alert("Message Sent."); 
-        this.resetForm()
-    }else if(resData.status === 'fail'){
-        alert("Message failed to send.")
+export default class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: '',
+          email: '',
+          number: '',
+          reason:'',
+          message: ''
+        }
     }
-    };
 
-    return (
-        <div>
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+    onNumberChange(event) {
+        this.setState({number: event.target.value})
+    }
+    onReasonChange(event) {
+        this.setState({reason: event.target.value})
+    }
+    onMessageChange(event) {
+        this.setState({message: event.target.value})
+    }
+
+    submitEmail(e){
+        e.preventDefault();
+        axios({
+          method: "POST", 
+          url:"/send", 
+          data: this.state
+        }).then((response)=>{
+          if (response.data.status === 'success'){
+              alert("Message Sent."); 
+              this.resetForm()
+          }else if(response.data.status === 'fail'){
+              alert("Message failed to send.")
+          }
+        })
+}
+resetForm(){
+        this.setState({name: '', email: '', number:'',reason:'', message: ''})
+}
+
+    render() {
+        return (
             <div className="Contact container">
                 <h1>Contact us</h1>
                 
-                <form  onSubmit={submitRequest}>
-                    <div className="input-group">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text">First and last name</span>
-                    </div>
-                    <input type="text" aria-label="First name" className="form-control" onChange={e => setEmail(e.target.value)} value={email} />
-                    <input type="text" aria-label="Last name" className="form-control" onChange={e => setMessage(e.target.value)} value={message} />
-                    </div>
-                    <button type="submit" className="primary-btn submit">Submit</button>
-                </form>
+                <Form.Group onSubmit={this.submitEmail.bind(this)} method="POST">
+                    <Form.Row>
+                        <Form.Label column="lg" lg={3}>
+                        Name
+                        </Form.Label>
+                        <Col>
+                        <Form.Control size="lg" type="text" placeholder="Name" required value={this.state.name} onChange={this.onNameChange.bind(this)} />
+                        </Col>
+                    </Form.Row>
 
+                    <Form.Row>
+                        <Form.Label column="lg" lg={3}>
+                        E-mail
+                        </Form.Label>
+                        <Col>
+                        <Form.Control size="lg" type="text" placeholder="E-mail" required value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                        </Col>
+
+                        <Form.Label column="lg" lg={3}>
+                        Phone Number
+                        </Form.Label>
+                        <Col>
+                        <Form.Control size="lg" type="text" placeholder="Phone Number" required value={this.state.number} onChange={this.onNumberChange.bind(this)}/>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Label column="lg" lg={3}>
+                        Reason for contacting us
+                        </Form.Label>
+                        <Col>
+                        <Form.Control size="lg" type="text" placeholder="Reason" required value={this.state.reason} onChange={this.onReasonChange.bind(this)}/>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Label column="lg" lg={2}>Message</Form.Label>
+                    <Form.Control as="textarea" rows={4} placeholder="Message" required value={this.state.message} onChange={this.onMessageChange.bind(this)}/>
+
+                    <Button variant="primary" size="lg" onClick={this.submitEmail}>
+                    Large button
+                    </Button>{' '}
+                </Form.Group>
             </div>
-        </div>
-    )
+        )
+    }
 }
