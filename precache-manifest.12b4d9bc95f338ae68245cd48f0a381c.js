e1a75e420c7821bf1e818debdfd66a6473c0a5bf
InfinityITDept <juan@usg-group.net>
2020-11-17 11:28:35 -0500

    Updates

---
+++ precache-manifest.12b4d9bc95f338ae68245cd48f0a381c.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949685179c172d7408a8c49ef31f7d17",
+    "url": "/Stern-X-React/index.html"
+  },
+  {
+    "revision": "9cc633c77d356594fd74",
+    "url": "/Stern-X-React/static/css/2.5f474a83.chunk.css"
+  },
+  {
+    "revision": "89480c3194e7ff1a5997",
+    "url": "/Stern-X-React/static/css/main.a6d552d0.chunk.css"
+  },
+  {
+    "revision": "9cc633c77d356594fd74",
+    "url": "/Stern-X-React/static/js/2.81b835c6.chunk.js"
+  },
+  {
+    "revision": "1fb97997b0ecd7d7d88e3d0a6b6bb983",
+    "url": "/Stern-X-React/static/js/2.81b835c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89480c3194e7ff1a5997",
+    "url": "/Stern-X-React/static/js/main.e554c770.chunk.js"
+  },
+  {
+    "revision": "9437d0c72c2988e3a43f",
+    "url": "/Stern-X-React/static/js/runtime-main.1cc9f4b9.js"
+  },
+  {
+    "revision": "c468e40d7506782997f1130078dfc14b",
+    "url": "/Stern-X-React/static/media/1 (1).c468e40d.jpg"
+  },
+  {
+    "revision": "c468e40d7506782997f1130078dfc14b",
+    "url": "/Stern-X-React/static/media/1-1.c468e40d.jpg"
+  },
+  {
+    "revision": "f5ea4c6984016da62e7f7a761c5dcda7",
+    "url": "/Stern-X-React/static/media/1-1.f5ea4c69.jpg"
+  },
+  {
+    "revision": "c468e40d7506782997f1130078dfc14b",
+    "url": "/Stern-X-React/static/media/1-2.c468e40d.jpg"
+  },
+  {
+    "revision": "f5ea4c6984016da62e7f7a761c5dcda7",
+    "url": "/Stern-X-React/static/media/1-2.f5ea4c69.jpg"
+  },
+  {
+    "revision": "c468e40d7506782997f1130078dfc14b",
+    "url": "/Stern-X-React/static/media/1-3.c468e40d.jpg"
+  },
+  {
+    "revision": "f5ea4c6984016da62e7f7a761c5dcda7",
+    "url": "/Stern-X-React/static/media/1-3.f5ea4c69.jpg"
+  },
+  {
+    "revision": "c468e40d7506782997f1130078dfc14b",
+    "url": "/Stern-X-React/static/media/1-4.c468e40d.jpg"
+  },
+  {
+    "revision": "f5ea4c6984016da62e7f7a761c5dcda7",
+    "url": "/Stern-X-React/static/media/1-4.f5ea4c69.jpg"
+  },
+  {
+    "revision": "73070fae2d10d3dfd6879f4f1a5dafaa",
+    "url": "/Stern-X-React/static/media/BannerPlaceholder-01.73070fae.jpg"
+  },
+  {
+    "revision": "e8c4a2f29470be97ec40f1d7e5ea6f39",
+    "url": "/Stern-X-React/static/media/BannerProducts.e8c4a2f2.png"
+  },
+  {
+    "revision": "f5ea4c6984016da62e7f7a761c5dcda7",
+    "url": "/Stern-X-React/static/media/Bleach Extra Strength-01.f5ea4c69.jpg"
+  },
+  {
+    "revision": "f193dbdf26f6d9c2e5ea338ccf25c8b1",
+    "url": "/Stern-X-React/static/media/Bleach Lavender-01.f193dbdf.jpg"
+  },
+  {
+    "revision": "1761e87f1809f87fbc0ef05974fdf090",
+    "url": "/Stern-X-React/static/media/Bleach Lemon-01.1761e87f.jpg"
+  },
+  {
+    "revision": "9226de1231bbdc9cc342280180531fa7",
+    "url": "/Stern-X-React/static/media/Bleach Regular-01.9226de12.jpg"
+  },
+  {
+    "revision": "c43efb198ca7ec053171f6a80622c99a",
+    "url": "/Stern-X-React/static/media/Bleach-01.c43efb19.jpg"
+  },
+  {
+    "revision": "0f7cb33a00944eec6e870c3d99f063a7",
+    "url": "/Stern-X-React/static/media/CleanHands-01.0f7cb33a.jpg"
+  },
+  {
+    "revision": "887b080bd2b4e8d4414e1f64dd1b4de4",
+    "url": "/Stern-X-React/static/media/Gloves-01.887b080b.jpg"
+  },
+  {
+    "revision": "552a9f49aff7733a66f59577d9745d24",
+    "url": "/Stern-X-React/static/media/Hand Sanitizer 13oz-01.552a9f49.jpg"
+  },
+  {
+    "revision": "8a86521a2946148d63f1dd8215f44946",
+    "url": "/Stern-X-React/static/media/Hand Sanitizer 16oz-01.8a86521a.jpg"
+  },
+  {
+    "revision": "37357795a98f5a32580f48c069cb86f0",
+    "url": "/Stern-X-React/static/media/Hand Sanitizer 64oz-01.37357795.jpg"
+  },
+  {
+    "revision": "34a164c7704c1315171fd79eb13c293f",
+    "url": "/Stern-X-React/static/media/Hand Sanitizer Liquid-01.34a164c7.jpg"
+  },
+  {
+    "revision": "cd4d2a454436c7e63f997f25c1aeae80",
+    "url": "/Stern-X-React/static/media/Headshot1.cd4d2a45.jpg"
+  },
+  {
+    "revision": "71fc825c13ca5cd83bcd5773492e2648",
+    "url": "/Stern-X-React/static/media/Headshot2.71fc825c.jpg"
+  },
+  {
+    "revision": "443299fb6474de4acb04df0ff5b7f57a",
+    "url": "/Stern-X-React/static/media/Headshot3.443299fb.jpg"
+  },
+  {
+    "revision": "4350c2560531d77c17aff59768759a05",
+    "url": "/Stern-X-React/static/media/Sanitizers-01.4350c256.jpg"
+  },
+  {
+    "revision": "c6b42e11983747848b68bc5765be0386",
+    "url": "/Stern-X-React/static/media/bannerbleach.c6b42e11.png"
+  },
+  {
+    "revision": "20d3e94846b0317ba981e9b4d3ecdabb",
+    "url": "/Stern-X-React/static/media/placeholder.20d3e948.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Stern-X-React/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Stern-X-React/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Stern-X-React/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Stern-X-React/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4350c2560531d77c17aff59768759a05",
+    "url": "/Stern-X-React/static/media/up.4350c256.png"
+  }
+]);
