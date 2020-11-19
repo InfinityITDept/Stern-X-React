a33e9f7295ee506c283de5b2963be79d24b595e8
InfinityITDept <juan@usg-group.net>
2020-11-19 11:52:24 -0500

    11/19/2020 @ 11:52PM Banner update

--- src/layout/components/Banner.js
+++ src/layout/components/Banner.js
@@ -1,58 +1,45 @@
 import React, { Component } from 'react';
 import "./Banner.scss";
-import BannerHS from "./images/BannerProducts.png";
-import BannerBleach from "./images/bannerbleach.png";
-
+import BannerRib1 from "./images/banner/rib1.png";
+import BannerRib2 from "./images/banner/rib2.png";
+import BannerHS from "./images/banner/hs.png";
+import BannerDisp from "./images/banner/dispenser.png";
+import BannerLogo from "./images/banner/logo100x40.png";
+import BannerBleach from "./images/banner/bleach.png";
+ 
 export default class Banner extends Component {
     render() {
         return (
-
-            <div id="banner" className="carousel slide div-1 w-100" data-ride="carousel">
+ 
+            <div id="banner" className="carousel carousel-fade div-1 w-100" data-ride="carousel">
                 <div className="carousel-inner">
+                    <img src={BannerRib1} className="img-fluid rib1" alt="Banner Img"/>
+                    
                     <div className="carousel-item active" data-interval="10000">
-
-
-                            <div className="col-12">
-                                <div className="div-2">
-                                    <p className="p2-1">CLEAN HANDS, SAVE LIVES</p>
-                                    <p className="p2-2">Soothing & Moisturizing</p>
-                                </div>
-                                <img src={BannerHS} className="img-fluid" alt="Banner Image" />
-                                <div className="div-3">
-                                    <p className="p3-1">GEL <br/>& <br/>LIQUID</p>
-                                </div>
-                                <div className="div-4">
-                                    <p className="p4-1">Kills 99% of Most GERMS</p>
-                                </div>
-                                <div className="div-5">
-                                    <p className="p5-1">75% <br/> ALCOHOL</p>
-                                </div>
-                            </div>
-
-
+                        <div className="banner-product">
+                            <img src={BannerHS} className="d-block" alt="Banner Image" />
+                            <div className="banner-logo-title"><img src={BannerLogo}/><span>HAND SANITIZER</span></div>
+                            <p>SOOTHING & MOISTURIZING</p>
+                        </div>
                     </div>
-                    <div class="carousel-item" data-interval="2000">
-        
-
-                            <div className="col-12">
-                                <div className="div-2">
-                                    <p className="p2-1">CLEAN HANDS, SAVE LIVES</p>
-                                    <p className="p2-2">Soothing & Moisturizing</p>
-                                </div>
-                                <img src={BannerBleach} className="img-fluid" alt="Banner Image" />
-                                <div className="div-3">
-                                    <p className="p3-1">GEL <br/>& <br/>LIQUID</p>
-                                </div>
-                                <div className="div-4">
-                                    <p className="p4-1">Kills 99% of Most GERMS</p>
-                                </div>
-                                <div className="div-5">
-                                    <p className="p5-1">75% <br/> ALCOHOL</p>
-                                </div>
-                            </div>
-
-
+ 
+                    <div className="carousel-item" data-interval="20000">
+                        <div className="banner-product">
+                            <img src={BannerDisp} className="d-block" alt="Banner Image" />
+                            <p>CLEAN HANDS, SAVE LIVES</p>
+                        </div>
                     </div>
+ 
+                    <div className="carousel-item" data-interval="20000">
+                        <div className="banner-product">
+                            <img src={BannerBleach} className="d-block" alt="Banner Image" />
+                            <div className="banner-logo-title"><img src={BannerLogo}/><span>BLEACH</span></div>
+                            <p>CLEANS & DESINFECTS</p>
+                        </div>
+                    </div>
+ 
+                    <img src={BannerRib2} className="img-fluid rib2" alt="Banner Img"/>
+ 
                 </div>
                 <a className="carousel-control-prev" href="#banner" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -63,7 +50,7 @@ export default class Banner extends Component {
                     <span className="sr-only">Next</span>
                 </a>
             </div>
-
+ 
         );
     }
 }
